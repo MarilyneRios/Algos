b242326dfd28d45d1b14441b8ffcfe5e4740c500
Rios marilyne <rios.marilyne@gmail.com>
2024-01-04 15:06:32 +0100

    done

---
+++ facile/findMultiple.js
@@ -0,0 +1,16 @@
+function Sol (nb){
+let sum = 0;
+if(nb<0) {
+    return 0;
+};
+
+for (let x = 3; x<nb; x++){
+    if (x % 3 === 0 ||x % 5 === 0){
+        sum += x;
+    }
+}
+
+console.log(sum)
+return sum;
+
+}
